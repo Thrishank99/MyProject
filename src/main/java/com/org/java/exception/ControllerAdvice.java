d3348b02eb28edab1ac5e67a5beadf0a3684494a
Thrishank99 <thrishank966@gmail.com>
2024-03-18 15:18:54 +0530

    fffdsds

--- src/main/java/com/org/java/exception/ControllerAdvice.java
+++ src/main/java/com/org/java/exception/ControllerAdvice.java
@@ -3,6 +3,7 @@ package com.org.java.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.MethodNotAllowedException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @org.springframework.web.bind.annotation.ControllerAdvice
@@ -19,8 +20,14 @@ public class ControllerAdvice extends ResponseEntityExceptionHandler {
 		
 	}
 	
+	@ExceptionHandler(MethodNotAllowedException.class)
+	public ResponseEntity<String> methodNotAllowedExceptionHandaller(MethodNotAllowedException  methodNotAllowedException ){
+		return new ResponseEntity<String>("please the chage the verb name as soon as possiable..",HttpStatus.BAD_REQUEST);
+
+	}
+	
 	@ExceptionHandler(NullPointerException.class)
-	public ResponseEntity<String> NullPointerException(NullPointerException nullPointerException){
+	public ResponseEntity<String> nullPointerException(NullPointerException nullPointerException){
 		return new ResponseEntity<String>("Given input no date availbale please check and change",HttpStatus.BAD_REQUEST);
 		
 	}
